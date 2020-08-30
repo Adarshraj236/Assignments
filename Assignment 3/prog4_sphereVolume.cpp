756d97bd0fdde08216fde60ec3de28064842f424
ADARSH RAJ <adarshraj236@gmail.com>
2020-08-31 02:48:57 +0530

    volume of sphere

---
+++ Assignment 3/prog4_sphereVolume.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class Volume
+{
+    public:
+    float radius;
+
+    Volume(float r){
+        cout<<"Enter the radius of the sphere = ";
+        cin>>r;
+        radius=r;
+    }
+    inline float sphereVolume(){
+        return ((4.0*3.14159*pow(radius,3))/3.0) ;
+    }
+};
+int main()
+{
+    float r;
+    Volume v1(r);
+    cout<<"Volume = ";
+    cout<< v1.sphereVolume()<<endl;
+}
